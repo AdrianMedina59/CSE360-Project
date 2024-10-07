e411b1bfc4557aa1b2d9cee4fbae2afee39a4027
markl <markl@LAPTOP-FDOTEDCR>
2024-10-07 13:03:15 -0700

    Added back in switch back to login in function. Will replace name when login page is completed.

--- Admin Page/src/application/Controller.java
+++ Admin Page/src/application/Controller.java
@@ -1,6 +1,12 @@
 package application;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -16,7 +22,20 @@ public class Controller
 	@FXML
 	private AnchorPane scenePane;
 	
-	public void logout(ActionEvent event)
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchbacktoLogin(ActionEvent event) throws IOException
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("/loginexample.fxml"));
+		stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene (root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void logout(ActionEvent event) throws IOException
 	{
 
 	
@@ -25,16 +44,20 @@ public class Controller
 	Alert alert = new Alert(AlertType.CONFIRMATION);
 	alert.setTitle("Logout");
 	alert.setHeaderText("Would you like to logout");
-	alert.setContentText("Do you want to save before you logout");
+	alert.setContentText("Hit Ok to log out. You will return back to login!!");
 			
 	if(alert.showAndWait().get() == ButtonType.OK)
 	{
 		stage = (Stage) scenePane.getScene().getWindow();
-		stage.close();
+		switchbacktoLogin(event);
 	}
 	
 	
 	
+	}
+	public void ListUsers(ActionEvent event)
+	{
+		
 	}
 
 
