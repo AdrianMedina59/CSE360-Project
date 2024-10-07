2721f577d85410ed32476b063efe2b32716e98ce
edwar <edwar@LAPTOP-O68SH0DK>
2024-10-07 11:56:13 -0700

    Updated Admin page colors

--- Admin Page/src/application/Main.java
+++ Admin Page/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -10,16 +12,14 @@ import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+	public void start(Stage primaryStage) throws IOException {
+		
+			Parent root = FXMLLoader.load(getClass().getResource("/GUI.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 	public static void main(String[] args) {
