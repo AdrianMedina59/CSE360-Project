9abcf4d46ceaeb3b84c2aa60ff8a903f50c298ea
Mloffman <mloffman@asu.edu>
2024-10-08 10:21:32 -0700

    Add files via upload
    With updated log out function and List Users

--- CSE_360_PROJECT/src/AdminPage/AdminPageController.java
+++ CSE_360_PROJECT/src/AdminPage/AdminPageController.java
@@ -1,6 +1,14 @@
 package AdminPage;
 import javafx.event.ActionEvent;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javafx.scene.Node;
+import ConfirmLogin.*;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -16,7 +24,28 @@ public class AdminPageController
 	@FXML
 	private AnchorPane scenePane;
 	
-	public void logout(ActionEvent event)
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	
+	public void printUsers() throws SQLException
+	{
+		ConfirmLogin.DataBaseHelper database = new ConfirmLogin.DataBaseHelper(); //data base
+		database.PrintUserTables();
+	}
+	
+	
+	public void switchbacktoLogin(ActionEvent event) throws IOException
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+		stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene (root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void logout(ActionEvent event) throws IOException
 	{
 
 	
@@ -25,17 +54,47 @@ public class AdminPageController
 	Alert alert = new Alert(AlertType.CONFIRMATION);
 	alert.setTitle("Logout");
 	alert.setHeaderText("Would you like to logout");
-	alert.setContentText("Do you want to save before you logout");
+	alert.setContentText("Hit Ok to log out. You will return back to login!!");
 			
 	if(alert.showAndWait().get() == ButtonType.OK)
 	{
 		stage = (Stage) scenePane.getScene().getWindow();
-		stage.close();
+
+		switchbacktoLogin(event);
+	}
+	
+	
+	}
+	
+	public void ListUsers(ActionEvent event) throws SQLException
+	{
+		printUsers();
+
+	}
+	
+	public void ResetUser(ActionEvent event)
+	{
+		
+	}
+	
+	public void InviteUser(ActionEvent event)
+	{
+		
+	}
+	
+	public void Delete(ActionEvent event)
+	{
+		
+	}
+	
+	public void AddorRemove(ActionEvent event)
+	{
+		
 	}
 	
 	
 	
-	}
+	
 
 
 }
