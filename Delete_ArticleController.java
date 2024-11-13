eb5e03d96566b577b1ec16a38135b52b32c98300
chris-vasquez28 <123095620+chris-vasquez28@users.noreply.github.com>
2024-11-13 13:27:36 -0700

    this is dumb idk y its letting me commit on eclipse
    details will be explained in meeting but yea

---
+++ Delete_ArticleController.java
@@ -0,0 +1,83 @@
+/**
+ * <p> Main class for continue to delete article </p>
+ * 
+ * <p> Description: This will be used to search through the database and delete an article by inputting the title .</p>
+ * 
+ * <p> Copyright: Mark Loffman © 2024 </p>
+ * 
+ * @author : Mark Loffman
+ * 
+ * @version 1.00		2024-10-28
+ *  
+ */
+
+package Article;
+
+import java.io.IOException;
+
+import ConfirmLogin.DataBaseHelper;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import ConfirmLogin.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+
+
+
+public class Delete_ArticleController 
+
+{
+	@FXML
+	private TextField Article_title; // JavaFX TextField to delete the article title
+	
+	@FXML
+	private AnchorPane DeleteArticle;
+	@FXML
+	private DataBaseHelper dataBase = new DataBaseHelper();
+	
+	
+	public void DeleteArticle() throws SQLException 
+	{
+		String title = Article_title.getText();
+			
+		dataBase.connectToDatabase();
+		
+		 Alert alert = new Alert(AlertType.CONFIRMATION);
+
+    	alert.setTitle("Delete");
+    	alert.setHeaderText("Would you like to delete the article");
+    	alert.setContentText("Hit submit to delete the article");
+    			
+    	if(alert.showAndWait().get() == ButtonType.OK)
+    	{
+    		
+    		if(dataBase.isValidArticleTitle(title)) 
+    		{
+                //delete article based on title
+              dataBase.deleteArticle(title);
+                
+            }
+    		
+    	}
+    	
+    	closeWindow();
+	}
+	
+	
+    void closeWindow()
+    {
+    	 // Code to close the window
+        Stage stage = (Stage) DeleteArticle.getScene().getWindow();
+        stage.close();
+    }
+}
+		
